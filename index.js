5501ef52ce85bf98baf8b9dd4947707a2198009f
Denis Tkachenko <uzicuss@gmail.com>
2020-05-09 16:07:24 +0300

    Fix format issue

--- index.js
+++ index.js
@@ -38,30 +38,10 @@ Release version: <%= jira.releaseVersions[0].name -%>
 <% }); -%>
 <% } %>
 
-Jira Tickets
----------------------
-<% tickets.all.forEach((ticket) => { %>
-  * <<%= ticket.fields.issuetype.name %>> - <%- ticket.fields.summary %>
-    [<%= ticket.key %>] <%= jira.baseUrl + '/browse/' + ticket.key %>
+<% tickets.all.filter((t) => !t.reverted).forEach((ticket) => { -%>
+  * [<%= ticket.key %>](<%= jira.baseUrl + '/browse/' + ticket.key %>) - <%- ticket.fields.summary %>
 <% }); -%>
-<% if (!tickets.all.length) {%> ~ None ~ <% } %>
-
-Other Commits
----------------------
-<% commits.noTickets.forEach((commit) => { %>
-  * <%= commit.slackUser ? '@'+commit.slackUser.name : commit.authorName %> - <<%= commit.revision.substr(0, 7) %>> - <%= commit.summary -%>
-<% }); -%>
-<% if (!commits.noTickets.length) {%> ~ None ~ <% } %>
-
-Pending Approval
----------------------
-<% tickets.pendingByOwner.forEach((owner) => { %>
-<%= (owner.slackUser) ? '@'+owner.slackUser.name : owner.email %>
-<% owner.tickets.forEach((ticket) => { -%>
-  * <%= jira.baseUrl + '/browse/' + ticket.key %>
-<% }); -%>
-<% }); -%>
-<% if (!tickets.pendingByOwner.length) {%> ~ None. Yay! ~ <% } %>
+<% if (!tickets.all.filter((t) => !t.reverted).length) {%> ~ None ~ <% } %>
 `;
 
 function generateReleaseVersionName() {
